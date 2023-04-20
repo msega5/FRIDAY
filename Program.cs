fff183f683ed40c377203f3029f3afface1d7181
Sergei Efanov <efanv_serg@mail.ru>
2023-04-20 15:59:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//Второй способ
+//Это мы не доделаи int day1 == ("Понедельник"), day2 == ("Вторник"), day1 == ("Понедельник"), 
+// ДЗ 
+// string num = Convert.ToString(Console.ReadLine());
+// Console.WriteLine(num[2]);
---
+++ Tast001_n^2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+//  (число умноженное  на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+Console.WriteLine(result);
+if (num > 0)
+{
+    Console.WriteLine("Было введено положительное число");
+}
+else if (num == 0)
+    Console.WriteLine("Был введен ноль");
+else
+    Console.WriteLine("Было введено отрицательное число");
