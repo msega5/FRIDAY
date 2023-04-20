291a46f3c1dbd76b87bd19799387f1662c19ba44
Sergei Efanov <efanv_serg@mail.ru>
2023-04-20 16:08:11 +0300

    Удалил лишние папки

--- Program.cs
+++
@@ -1,5 +0,0 @@
-﻿//Второй способ
-//Это мы не доделаи int day1 == ("Понедельник"), day2 == ("Вторник"), day1 == ("Понедельник"), 
-// ДЗ 
-// string num = Convert.ToString(Console.ReadLine());
-// Console.WriteLine(num[2]);
