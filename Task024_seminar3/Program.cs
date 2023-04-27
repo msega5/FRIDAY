878f29a14c438b760dc5080245c2b2dc92fe7c9d
Sergei Efanov <efanv_serg@mail.ru>
2023-04-27 22:54:25 +0300

    Добавил задачу 026

--- Task024_seminar3/Program.cs
+++ Task024_seminar3/Program.cs
@@ -1,56 +1,9 @@
-﻿// // Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
-// // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
-
-// //Второй способ с методом
-// // void CheckKoord(int x, int y)
-// // {
-// //     if (x > 0 && y > 0)
-// //         Console.WriteLine("Это четверть 1");
-// //     else if (x > 0 && y < 0)
-// //         Console.WriteLine("Это четверть 4");
-// //     else if (x < 0 && y < 0)
-// //         Console.WriteLine("Это четверть 3");
-// //     else if (x < 0 && y < 0)
-// //         Console.WriteLine("Это четверть 2");
-// //     else
-// //         Console.WriteLine("Точка лежит на координатной оси");
-// // }
+﻿
 
 
 
 
-// // Console.WriteLine("Введите коррдинату X: ");
-// // int x1 = Convert.ToInt32(Console.ReadLine());
-// // Console.WriteLine("Введите коррдинату Y: ");
-// // int y1 = Convert.ToInt32(Console.ReadLine());
-// // CheckKoord(x1, y1);
 
-
-// //Третий способ с возвратом
-// // int CheckKoord(int x, int y)
-// // {
-// //     int result = 0;
-// //     if (x > 0 && y > 0)
-// //         result = 1;
-// //     else if (x > 0 && y < 0)
-// //         result = 4;
-// //     else if (x < 0 && y < 0)
-// //         result = 3;
-// //     else if (x < 0 && y < 0)
-// //         result = 2;
-// //     return result;
-// // }
-
-
-
-
-// // Console.WriteLine("Введите коррдинату X: ");
-// // int x = Convert.ToInt32(Console.ReadLine());
-// // Console.WriteLine("Введите коррдинату Y: ");
-// // int y = Convert.ToInt32(Console.ReadLine());
-// // CheckKoord(x, y);
-// // int rez = CheckKoord(x, y);
-// // Console.WriteLine($"Это четверть № {rez}");
 
 
 
---
+++ Task026_по_xy_определить_четверть_метод_int/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+//Третий способ с возвратом
+
+int CheckKoord(int x, int y)
+{
+    int result = 0;
+    if (x > 0 && y > 0)
+        result = 1;
+    else if (x > 0 && y < 0)
+        result = 4;
+    else if (x < 0 && y < 0)
+        result = 3;
+    else if (x < 0 && y < 0)
+        result = 2;
+    return result;
+}
+
+
+
+
+Console.WriteLine("Введите коррдинату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите коррдинату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+CheckKoord(x, y);
+int rez = CheckKoord(x, y);
+Console.WriteLine($"Это четверть № {rez}");
