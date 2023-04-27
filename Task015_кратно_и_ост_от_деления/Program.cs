1c3624e63069514ae775c792c6ebbb4397ada75a
Sergei Efanov <efanv_serg@mail.ru>
2023-04-28 00:04:15 +0300

    Почистил ветку

---
+++ Task015_кратно_и_ост_от_деления/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая будет принимать
+// на вход два числа и выводить, является ли второе
+// число кратным первому. Если число 2 не кратно числу 1,
+// то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int ost = num1 % num2;
+if (ost == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно остаток {ost}");
+}
+
+//Второй вариант
+// Console.WriteLine("Введите 2 числа"); 
+// int num1 = Convert.ToInt32(Console.ReadLine()); 
+// int num2 = Convert.ToInt32(Console.ReadLine()); 
+// int div = num1 % num2; 
+// if( div == 0) 
+//     Console.WriteLine("Число кратно"); 
+// else 
+//     Console.WriteLine($"Число не кратно, остаток от деления =  {div}");
