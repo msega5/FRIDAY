6cc571e5ef5fb9df80869f39ef9b6252fa5add81
Sergei Efanov <efanv_serg@mail.ru>
2023-04-28 00:44:37 +0300

    Добавил задачи 1-7

---
+++ Task004_numToDay_case/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+// Второй способ switch (выражение)
+
+Console.WriteLine("Введите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Введите число от 1 до 7");
+        break;
+}
---
+++ Task007_3rd_num_from_right/Program.cs
@@ -0,0 +1,14 @@
+﻿// На входе число любой разрядности. Надо вывести третью справа цифру 56899 -> 8
+
+Console.WriteLine("Введите число");
+long num = Convert.ToInt64(Console.ReadLine());
+long result = 0;
+if (num < 100)
+{
+    Console.WriteLine("Неверное число. Введите трёхзначное число");
+}
+else
+{
+    result = (num / 100) % 10;
+}
+Console.WriteLine(result);
