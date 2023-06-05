a067dbdf683ddb838a6eb2a71123e761e819fd71
Sergei Efanov <efanv_serg@mail.ru>
2023-06-05 13:12:07 +0300

    Добавил задачи семинара 8

---
+++ Task061_замена_строк_на_столбцы_массива/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 53: Задайте двумерный массив.
+// Напишите программу, которая поменяет местами
+// первую и последнюю строку массива.
+
+// Задача 55: Задайте двумерный массив.
+// Напишите программу, которая заменяет строки на
+// столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-20, 20);
+}
+
+void PrintArray(int[,] array)
+{
+    System.Console.WriteLine("Исходный массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            System.Console.Write($"{array[i, j],3}\t");
+        System.Console.WriteLine();
+    }
+}
+
+void ChangeArrayString(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = i; j < array.GetLength(1); j++)
+        {
+            (array[i, j], array[j, i])
+          = (array[j, i], array[i, j]);
+        }
+    }
+}
+
+
+
+
+Console.Clear();
+System.Console.Write("Введите размер массива: ");
+int rows = Convert.ToInt32(System.Console.ReadLine());
+int[,] array = new int[rows, rows];
+FillArray(array);
+PrintArray(array);
+ChangeArrayString(array);
+PrintArray(array);
---
+++ Task062_частотный_словарь/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            System.Console.Write($"{array[i, j],3}    ");
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[] Quant(int[,] array)
+{
+    int[] arrayOne = new int[10];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            // if(array[i,j]==0)
+            // arrayOne[0]++;
+            // if(array[i,j]==1)
+            // arrayOne[1]++;
+            // if(array[i,j]==2)
+            // arrayOne[2]++;
+            // if(array[i,j]==3)
+            // arrayOne[3]++;
+
+            arrayOne[array[i, j]]++;
+        }
+    }
+    return arrayOne;
+}
+
+void PrintAr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] != 0)
+            System.Console.WriteLine($"В данном массиве число {i} встречается: {array[i]} раз");
+
+    }
+}
+
+// int[] CountPosition(int[,] array)
+// {
+//     int[] count = new int[10];
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         for (int k = 0; k < count.Length; k++)
+//         {
+//             if (k == array[i, j])
+//                 {count[k]++;
+//                 k++;}  
+//         }
+
+//     }
+//     return count;
+
+// }
+Console.Clear();
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+int[] myarray = Quant(array);
+PrintAr(myarray);
